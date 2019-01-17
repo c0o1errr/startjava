91f94767a161c4fc269848d9b21ce9359db1efa8
unknown <d0theshka@gmail.com>
2019-01-17 20:44:28 +0300

    wolfTest

---
+++ lesson2/WolfTest.java
@@ -0,0 +1,12 @@
+public class WolfTest {
+
+	public static void main(String[] args) {
+		Wolf wolfOne = new Wolf();
+		System.out.println("Волчонка зовут = " + wolfOne.getNameWolf());
+		// wolfOne.nameWolf = "котенок";
+		System.out.println (wolfOne.getNameWolf());
+		System.out.println (wolfOne.useHawl());
+
+
+	}
+}
