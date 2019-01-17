5107b4098326c7eca24ba38c91284791b3e36dd3
unknown <d0theshka@gmail.com>
2019-01-17 22:10:32 +0300

    закорючки

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,6 +1,6 @@
 public class Wolf {
 
-	private String nameWolf = "Собака";
+	private String nameWolf = "Бублик";
 	private String sex = "male";
 	private String color = "grey";
 	private float weight = 12.5f;
@@ -19,6 +19,38 @@ public class Wolf {
 
 	public String getSex() {
 		return sex;
+	}
+
+	void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getColor(){
+		return color;
+	}
+
+	void setColor (String color){
+		this.color = color;
+	}
+
+	public getWeight() {
+		return weight;
+
+	}
+
+	void setWeight(float weight){
+		this.weight = weight;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
+	void setAge( int age) {
+		if (age > 8) {
+			System.out.println("Некорретный возраст");
+		} else {
+		this.age = age;
 	}
 
 
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -3,7 +3,7 @@ public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
 		System.out.println("Волчонка зовут = " + wolfOne.getNameWolf());
-		// wolfOne.nameWolf = "котенок";
+		// wolfOne.nameWolf = "ГЄГ®ГІГҐГ­Г®ГЄ";
 		System.out.println (wolfOne.getNameWolf());
 		System.out.println (wolfOne.useHawl());
 
