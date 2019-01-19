275285a07d8d1778c5f781a33bf5176132c12a3f
unknown <d0theshka@gmail.com>
2019-01-19 19:40:07 +0300

    исправления

--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -1,12 +1,12 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
-		wolfOne.setNameWolf ("Petr");
+		wolfOne.setName("Petr");
 		wolfOne.setSex("male");
 		wolfOne.setColor("brown");
 		wolfOne.setWeight(12);
 		wolfOne.setAge(9);
-		System.out.println("Wolf's name is = " + wolfOne.getNameWolf());
+		System.out.println("Wolf's name is = " + wolfOne.getName());
 		System.out.println("Sex = " + wolfOne.getSex());
 		System.out.println("Color = " + wolfOne.getColor());
 		System.out.println("Weight = " + wolfOne.getWeight());
