1445b7c9d2858758008b54dfa4085f4ef7835ce5
unknown <d0theshka@gmail.com>
2019-01-24 18:41:27 +0300

    исправления

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 public class CalculatorTest {
 	
 	public static void main (String [] args) {
-		// Считываю введенные числа и передаю через сеттер
-			Scanner scan = new Scanner(System.in);	
-			Calculator calculatorOne = new Calculator();	
-			System.out.print("Input first number: " + calculatorOne.getFirst());
-			calculatorOne.setFirst(scan.nextInt());
+		// I read the entered numbers and pass through the setter
+
+		Scanner scan = new Scanner(System.in);	
+		Calculator calculatorOne = new Calculator();	
+		System.out.print("Input first number: " + calculatorOne.getFirst());
+		calculatorOne.setFirst(scan.nextInt());
 		
 		
-			System.out.print("Enter the sign of the matematical operation: (+, -, *, /): " + calculatorOne.getOperator());		
+		System.out.print("Enter the sign of the matematical operation: (+, -, *, /): " + calculatorOne.getOperator());		
         	calculatorOne.setOperator(scan.next().charAt(0));  
 
         	System.out.print("Insert second number: " + calculatorOne.getSecond());
-			calculatorOne.setSecond(scan.nextInt());
+		calculatorOne.setSecond(scan.nextInt());
 
-			System.out.println("Результаты подсчетов = " + calculatorOne.calculate());
+		System.out.println("Результаты подсчетов = " + calculatorOne.calculate());
 
 	}
 } 
