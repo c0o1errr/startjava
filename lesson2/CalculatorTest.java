52e27e4d7cd70f86d19ea137d241093d329b0b9d
unknown <d0theshka@gmail.com>
2019-01-25 21:28:40 +0300

    добавил да/нет

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -5,19 +5,36 @@ public class CalculatorTest {
 	public static void main (String [] args) {
 		// I read the entered numbers and pass through the setter
 
-		Scanner scan = new Scanner(System.in);	
-		Calculator calculatorOne = new Calculator();	
-		System.out.print("Input first number: " + calculatorOne.getFirst());
-		calculatorOne.setFirst(scan.nextInt());
+		Scanner scan = new Scanner(System.in);
+		Start();
+		while (true) {
+						
+			System.out.println("Want to continue ? enter yes/no" );
+			String again = scan.nextLine();
+
+			if ("yes".equals(again)) {
+				Start();
+			} else if ("no".equals(again)) {
+				break;
+			} else {
+				System.out.println("Want to continue ? enter yes/no" );
+			}
+		}
+	}	
+
+	private static void Start() {
+		Scanner scnr = new Scanner(System.in);
+		Calculator counts = new Calculator();
+    	
+		System.out.print("Input first number: " );
+		counts.setFirst(scnr.nextInt());	
 		
-		
-		System.out.print("Enter the sign of the matematical operation: (+, -, *, /): " + calculatorOne.getOperator());		
-        	calculatorOne.setOperator(scan.next().charAt(0));  
+		System.out.print("Enter the sign of the matematical operation: (+, -, *, /, ^, % (остаток от деления)): ");
+        counts.setOperator(scnr.next().charAt(0));
 
-        	System.out.print("Insert second number: " + calculatorOne.getSecond());
-		calculatorOne.setSecond(scan.nextInt());
+        System.out.print("Insert second number: " );
+		counts.setSecond(scnr.nextInt());
 
-		System.out.println("Counting results = " + calculatorOne.calculate());
-
-	}
-} 
+		System.out.println("Counting results = " + counts.calculate());
+	}	
+}
