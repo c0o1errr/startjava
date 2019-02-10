c97041479d4c24e30ef5bc9f495065251a1f5c24
unknown <d0theshka@gmail.com>
2019-02-10 18:18:10 +0300

    информация по двум егерям

--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,9 +1,13 @@
 public class JaegerTest {
 	public static void main (String [] args) {
-		JaegerOne jaegerFirst = new JaegerOne();
-		JaegerTwo jaegerSecond = new JaegerTwo();
-		
-		System.out.println("Model first jaeger " + jaegerFirst.getModelNameOne());
-		System.out.println("Model second jaeger " + jaegerSecond.getModelNameTwo());
+		Jaeger jaeger = new Jaeger();
+
+		System.out.println("Model first jaeger : " + jaeger.getModelNameOne());
+		System.out.println("Model second jaeger : " + jaeger.getModelNameTwo());
+		System.out.println("Mark first jaeger : " + jaeger.getMarkOne());
+		System.out.println("Mark second jaeger : " + jaeger.getMarkTwo());
+		System.out.println("Origin first jaeger : " + jaeger.getOriginOne());
+		System.out.println("Origin second jaeger : " + jaeger.getOriginTwo());
+
 	}
 }
