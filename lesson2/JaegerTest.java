cbfbbbe34c492db847a02317d36960e0639e33f7
unknown <d0theshka@gmail.com>
2019-02-15 14:29:23 +0300

    имена через сеттеры

--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,13 +1,10 @@
 public class JaegerTest {
 	public static void main (String [] args) {
-		Jaeger jaeger = new Jaeger();
-
-		System.out.println("Model first jaeger : " + jaeger.getModelNameOne());
-		System.out.println("Model second jaeger : " + jaeger.getModelNameTwo());
-		System.out.println("Mark first jaeger : " + jaeger.getMarkOne());
-		System.out.println("Mark second jaeger : " + jaeger.getMarkTwo());
-		System.out.println("Origin first jaeger : " + jaeger.getOriginOne());
-		System.out.println("Origin second jaeger : " + jaeger.getOriginTwo());
-
+		Jaeger jaegerOne = new Jaeger();
+		Jaeger jaegerTwo = new Jaeger();
+		jaegerOne.setModelName("Matador Fury");
+		jaegerTwo.setModelName("Chrome Brutus");
+		System.out.println("Model first jaeger : " + jaegerOne.getModelName());
+		System.out.println("Model second jaeger : " + jaegerTwo.getModelName());
 	}
 }
