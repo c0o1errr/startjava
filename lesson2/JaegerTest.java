bda63cfaa719f703431a254898a697b3b9e3e566
unknown <d0theshka@gmail.com>
2019-02-15 16:04:12 +0300

    конструтор

--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,10 +1,5 @@
 public class JaegerTest {
 	public static void main (String [] args) {
-		Jaeger jaegerOne = new Jaeger();
-		Jaeger jaegerTwo = new Jaeger();
-		jaegerOne.setModelName("Matador Fury");
-		jaegerTwo.setModelName("Chrome Brutus");
-		System.out.println("Model first jaeger : " + jaegerOne.getModelName());
-		System.out.println("Model second jaeger : " + jaegerTwo.getModelName());
+		Jaeger jaegerOne = new Jaeger("Matador Fury", "Mark3", "Canada");		
 	}
 }
