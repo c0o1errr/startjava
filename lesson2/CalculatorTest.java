5da444daa52dceb47e72dfa931471a15f1459681
unknown <d0theshka@gmail.com>
2019-02-20 12:54:54 +0300

    убрал двойной start и nextLine

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-public class CalculatorTest {	
-	public final static Scanner scan = new Scanner(System.in);
+public class CalculatorTest {
+	public static final Scanner scan = new Scanner(System.in);
+	public static final String again = scan.nextLine();
 	public static void main (String [] args) {
 		// I read the entered numbers and pass through the setter
-		start();	
-		String again = "";	
-		do {							
-			String repeat = scan.nextLine();			
-			if ("yes".equals(repeat)) {
-				start();							
-			} else { 
-				System.out.println("Want to continue ? enter yes/no");
-			}
-		} while (again == "no");				
+		do {	
+			start();
+			String again = scan.nextLine();
+			if ("no".equals(again)) {
+				break;				
+			} 
+		} while (true);				
 	}	
 
 	private static void start() {		
-		Calculator calculate = new Calculator();    	
-		System.out.print("Input first number: " );
+		Scanner scan = new Scanner(System.in);		
+		Calculator calculate = new Calculator();  
+		System.out.print("Input first number: " );  		
 		calculate.setFirst(scan.nextInt());		
 		System.out.print("Enter the sign of the matematical operation: (+, -, *, /, ^, % (remainder of the division)): ");
         	calculate.setOperator(scan.next().charAt(0));
         	System.out.print("Insert second number: " );
 		calculate.setSecond(scan.nextInt());
 		System.out.println("Counting results = " + calculate.calculate());
+		System.out.println("Want to continue ? enter yes/no" );
 	}		
 }
