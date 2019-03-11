4285d4b4d456c9e62786aa73dd11e789e3845333
unknown <d0theshka@gmail.com>
2019-03-11 17:32:37 +0300

    добавлен конструтор

--- lesson2/Player.java
+++ lesson2/Player.java
@@ -2,24 +2,14 @@ public class Player {
 	private String name;	
 	private int number;
 
-
-	public String getName() {
-		return name;
+	public Player (String name) {
+       this.name = name;
 	}
-
-	public void setName(String name) {
-		if (name == "") {
-			System.out.println("Incorrect name ");
-		} else {
-			this.name = name;
-		}		
-	}	
 
 	public int getNumber() {
 		return number;
 	}
-
-	public void setNumber(int number) {
+	public void setNumber( int number) {
 		this.number = number;
 	}
 }
