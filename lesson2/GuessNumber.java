0da4432456dab9102f6767d2598e9d7556b583d9
unknown <d0theshka@gmail.com>
2019-03-11 12:56:57 +0300

    удалил лишние файлы и добавил корректный

---
+++ lesson2/GuessNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class GuessNumber2 {
+	public static final Scanner sc = new Scanner(System.in);
+	// Randomly generated computer numbers
+	public int randomize() {
+		int minRnd = 0; // initial value of the range
+		int maxRnd = 100; // range end value
+		int random = minRnd+(int)(Math.random()*maxRnd);
+
+		while (true) {
+			Player numberPlayerOne = new Player();
+			System.out.println("The first player enters the number: ");
+			int numberPlayerOne = sc.nextInt();
+			Player numberPlayerTwo = new Player();
+			System.out.println("The second player enters the number: ");
+			int numberPlayerTwo = sc.nextInt();
+
+			if (numberPlayerOne==random) {
+				System.out.println("The first player guessed the number !!! Guess number = " + random);
+				break;
+			}
+			if (numberPlayerTwo==random) {
+				System.out.println("The second player guessed the number !!! Guess number = " + random);
+				break;
+			} else {
+				System.out.println("Nobody guessed, try again. ");
+			} if (numberPlayerOne < random) {
+				System.out.println("The number is less than the guess ");
+			} if (numberPlayerTwo < random) {
+				System.out.println("The number is less than the guess ");
+			} if (numberPlayerOne > random) {
+				System.out.println("The number is greater than the guess ");
+			} if (numberPlayerTwo > random) {
+				System.out.println("The number is greater than the guess ");
+			}
+		}		
+	}
+}
