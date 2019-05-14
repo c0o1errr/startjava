fc3a079419e070ccf0cf62a288a5bf4e00445c66
unknown <d0theshka@gmail.com>
2019-05-14 13:53:02 +0300

    последняя версия

--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,5 +1,18 @@
+import java.util.Scanner; 
+
 public class JaegerTest {
 	public static void main (String [] args) {
-		Jaeger jaegerOne = new Jaeger("Matador Fury", "Mark3", "Canada");		
+		Jaeger gipsyAvenger = new Jaeger ( "GipsyAvenger", "Mark-6", "Pan Pacifice Defense Corps", 81.8f, 2.004f);				
+		Jaeger gipsyDanger = new Jaeger ("Gipsy Danger", "Mark-3", "USA", 79.2f, 1.98f);
+
+		System.out.println(gipsyAvenger);
+		System.out.println(gipsyDanger);
+
+		gipsyDanger.drift();
+		System.out.println(gipsyAvenger.scanKaiju());
+		gipsyDanger.useWeapon();
+		gipsyAvenger.move();
+		
+		System.out.println(gipsyDanger.toString());
 	}
 }
